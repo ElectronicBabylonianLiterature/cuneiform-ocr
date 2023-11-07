7383155e0881848c1bb340e934a0442a2901ae18
Yunus <yunus.cobanoglu@yahoo.de>
2023-11-07 13:41:14 +0100

    fix mddet

--- mmdetection/mmdet/__init__.py
+++ mmdetection/mmdet/__init__.py
@@ -13,10 +13,7 @@ mmengine_minimum_version = '0.7.1'
 mmengine_maximum_version = '1.0.0'
 mmengine_version = digit_version(mmengine.__version__)
 
-assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version < digit_version(mmcv_maximum_version)), \
-    f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv>={mmcv_minimum_version}, <{mmcv_maximum_version}.'
+
 
 assert (mmengine_version >= digit_version(mmengine_minimum_version)
         and mmengine_version < digit_version(mmengine_maximum_version)), \
