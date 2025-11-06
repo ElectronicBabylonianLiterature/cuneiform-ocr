64ad0c4f84cc48d6e7d00975e8b84f6e58123255
JebediahC <572301036@qq.com>
2025-11-06 18:00:00 +0100

    Do inferences

--- job_do_anatations.sh
+++ job_do_anatations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p lrz-v100x2
+#SBATCH -p lrz-dgx-1-p100x8 
 #SBATCH --gres=gpu:1
-#SBATCH -o log_anno_%j.log                   # File to store standard output
-#SBATCH -e log_anno_%j.log                   # File to store standard error
+#SBATCH -o log_anno_exp-3%j.log                   # File to store standard output
+#SBATCH -e log_anno_exp-3%j.log                   # File to store standard error
 #SBATCH --time=24:00:00                 # Set a time limit
 
 echo "Start on $(hostname) at $(date)"  # Run outside of srun
@@ -10,13 +10,14 @@ echo "Start on $(hostname) at $(date)"  # Run outside of srun
 export PATH=$HOME/.local/bin:$PATH
 export PYTHONPATH=$HOME/cuneiform-ocr/mmdetection:$PYTHONPATH
 
-cd $HOME/cuneiform-ocr/
+cd $HOME/erc-src/cuneiform-ocr/
 
 srun set_up_env.sh
 
 
 srun pip install "numpy<2.0.0"
 
-srun python do_annotations.py
+srun TAG=exp-3 THRESHOLD=0.7 Y_THRESHOLD=35 python do_annotations.py
+
 
 echo "End on $(hostname) at $(date)"
